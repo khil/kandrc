c8fdfefb9d8f018b80998f61d82843849f8a8552
khil <khildreth@ThinkCentre.home>
2024-10-17 20:56:35 -0400

    including minor tweaks to code, not functionality changes

--- ch1/ex1-13.c
+++ ch1/ex1-13.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
 /*
- * Exercise 1-12.  Write a program that prints a histogram of the lengths of words in
+ * Exercise 1-13.  Write a program that prints a histogram of the lengths of words in
  * its input.  Its easy to draw a histogram with bars horizontal; a vertical
  * orientation is more challenging.
 */
 
 #define IN 1
 #define OUT 2
-#define MAX_WORD_LENGTH 26
+#define MAX_WORD_LEN 26
 
 int main() {
-	int c, i, state, start_i;
-	int word_len[MAX_WORD_LENGTH];
+	int c, i, state, strt_i;
+	int word_len[MAX_WORD_LEN];
 	state = OUT;
 
 	/* init the array to zero */
-	for(i = 0; i < MAX_WORD_LENGTH; i++) {
+	for(i = 0; i < MAX_WORD_LEN; i++) {
 		word_len[i] = 0;
 	}
 
@@ -25,21 +25,21 @@ int main() {
 		if (c == ' ' || c == '\t' || c == '\n') {
 			if (state == IN) {
 				state = OUT;
-				word_len[i - start_i]++;
+				word_len[i - strt_i]++;
 			}
 		}
 		else {
 			if (state == OUT) {
 				state = IN;
-				start_i = i;
+				strt_i = i;
 			}
 		}
 	}
 
 	/* write the results to the stdout */
-	for(i = 1; i < MAX_WORD_LENGTH; i++) {
+	for(i = 1; i < MAX_WORD_LEN; i++) {
 		printf("%2d> ", i);
-		for (start_i = 0; start_i < word_len[i]; start_i++) {
+		for (strt_i = 0; strt_i < word_len[i]; strt_i++) {
 			putchar('|');
 		}
 		putchar('\n');
--- ch1/ex1-6.c
+++ ch1/ex1-6.c
@@ -2,7 +2,6 @@
 
 /*
  * Exercise 1-6.  Verify that the expression getchar() != EOF is 0 or 1.
- *
 */
 
 int main() {
