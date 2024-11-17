15eaf6be9c9465b6da078dfd41b10baacb75c07e
khil <khildreth@ThinkCentre.home>
2024-11-16 21:14:48 -0500

    corrected exercise number

--- ch2/ex2-03.c
+++ ch2/ex2-03.c
@@ -3,7 +3,7 @@
 #include <ctype.h>
 
 /*
-** Exercise 4.3. Write a function htoi(s, which converts a string of hexadecimal
+** Exercise 2.3. Write a function htoi(s, which converts a string of hexadecimal
 ** digits (including an optional 0x or 0X) into its equivalent integer value.
 ** The allowable digits are 0 through 9 and a thorugh f and A through F.
 **
