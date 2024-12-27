005208096ec2a340c8a9fe0b465f41e9dd912342
Kirk Hildreth <kirk.hildreth@gmail.com>
2024-12-27 09:20:13 -0500

    Modified comments to matche operations in the code.

--- ch2/ex2-07.c
+++ ch2/ex2-07.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <assert.h>
 
 /*
@@ -25,7 +24,6 @@ unsigned _invert(unsigned x, unsigned p, unsigned n) {
     **      n - For x, the number of bits to the right of p to replace. For y, the number of bits,
     **          from the rightmost bit to copy into x. Valid values 0 to p + 1. Note: if n = Zero
     **          there's nothing to do.
-    **      y - Source value from which to get n bits.
     **
     **  Assuming (using 8 bits for simplicity):
     **      x = 10011011 (155)
@@ -35,14 +33,14 @@ unsigned _invert(unsigned x, unsigned p, unsigned n) {
     **  1. Validate the ranges of p & n.
     **  2. Create a mask to isolate the bits we want to retain from x. To achieve this create two
     **     temporary masks, one for the left side bits and one for the right. The two temporary
-    **     masks will be combined into a single mask using a bitwise AND operation.
+    **     masks will be combined into a single mask using a bitwise OR operation.
     **     a. The left mask should retain the bit(s) on left side of p+1. To make the mask start
     **        with a NOT 0 (~0), then shifting the bits left p+1 positions; the result should be:
     **        10000000
     **     b. The right mask needs to retain the bit(s) to the right of the index p+1-n. To make
     **        the mask start with (~0) and shifting the bits to the right by p+1-n. i.e.,
     **        11110000. Then bitwise NOT the result, i.e., 00001111.
-    **     c. The next step to create the mask is to bitwise OR the left and right masks with the
+    **     c. The next step is to create the mask is bitwise ORing the left and right masks, the
     **        result being: 10001111.
     **     d. The final step is to bitwise NOT(~) the mask created in 3c to make the mask 01110000.
     **  3. Bitwise XOR x and the mask created in step 3 returning the result, i.e., 11101011.
@@ -51,9 +49,10 @@ unsigned _invert(unsigned x, unsigned p, unsigned n) {
         return x;
 
     if ((p >= 0 && p <= (sizeof(unsigned) * 8) - 1) && (n >= 1 && n <= p+1)) { /* 1 */
-        unsigned left_bits = ~0 << (p+1);                               /* 2a */
-        unsigned right_bits = ~(~0 << (p+1-n));                         /* 2b */
-        return x ^ ~(left_bits | right_bits);                           /* 3, 2c, & 2d */
+        unsigned left_bits = ~0 << (p+1);            /* 2a */
+        unsigned right_bits = ~(~0 << (p+1-n));      /* 2b */
+        unsigned mask = ~(left_bits | right_bits);   /* 2c, & 2d */
+        return x ^ mask;                             /* 3 */
     }
     else
         return 0;
@@ -69,10 +68,11 @@ int main() {
     assert((invert(155, 6, 0) == 155) && (_invert(155, 6, 0) == 155));    
 
     assert((invert(155, 6, 3) == 235) && (_invert(155, 6, 3) == 235));    
-    /* std -> x=1(001)1011 : 1(110)1011 */
+
+    /* std -> x=1001(10)11 : 1001(01)11 */
     assert((invert(235, 3, 2) == 231) && (_invert(235, 3, 2) == 231));    
 
-    /* std test -> x=00000(01)0 : equal to x */
+    /* std test -> x=00000(01)0 : 00000(10)0 */
     assert((invert(2, 2, 2) == 4) && (_invert(2, 2, 2) == 4));    
 
     /* std test -> x=0000(1)111 : 0000(0)111 */
@@ -81,7 +81,7 @@ int main() {
     /* std test -> x=0011(01)11 : 0011(10)11 */
     assert((invert(55, 3, 2) == 59) && (_invert(55, 3, 2) == 59));
     
-    /* std test -> x=(00110111) : equal to y */
+    /* std test -> x=(00110111) : (11001000) */
     assert((invert(55, 7, 8) == 200) && (_invert(55, 7, 8) == 200)); 
 
     /* std test -> x=1111(1111) : 1111(0000) */
