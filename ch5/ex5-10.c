f80813e503822694fb45d48b0808bc5a97042d82
Kirk Hildreth <kirk.hildreth@gmail.com>
2025-07-26 23:47:42 -0400

    final revision to exercise 5-10

--- ch5/ex5-10.c
+++ ch5/ex5-10.c
@@ -1,33 +1,49 @@
 #include <ctype.h>
-#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
 
+/*
+ * Exercise 5-10. Write the program expr, which expruates a reverse Polish expression from the
+ * command line, where each operator or operand is a separate argument. For example,
+ * 
+ *     expr 2 3 4 + *
+ * expruates 2 * (3+4).
+ */ 
+
+
 #define ERROR 1
 #define MAXPARMS 10
-
 #define CHARACTER 0
 #define OPERATOR 1
 #define NUMBER 2
 
 
-void gen_eq(char *s1, char *s2, char *op, char *res) {
-    strcat(res, "(");
-    strcat(res, s1);
-    strcat(res, op);
-    strcat(res, s2);
-    strcat(res, ")"); 
+float expr(const float n1, const float n2, const char op) {
+    switch (op) {
+        case '+':
+            return n1 + n2;
+        case '-':
+            return n1 - n2;
+        case '*':
+            return n1 * n2;
+        case '/':
+            return n1 / n2;
+        case '%':
+            return (int)n1 % (int)n2;
+    }
+    return 0;
 }
 
 
 uint8_t gettype(char *str) {
     char *p = str;
-
+    
     if (strlen(p) == 1) {
         if (isdigit(*p))
             return NUMBER;
-        return (*p == '+' || *p == '-' || *p == '*' || *p == '/');
+        return (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '%');
     }
     else {
         if (*p == '-' || *p == '+')
@@ -43,27 +59,21 @@ uint8_t gettype(char *str) {
 
 
 int32_t main(int32_t argc, char **argv) {
-
     if (argc < 3) {
         puts("insufficient parameters");
-    return ERROR;
+        return ERROR;
     }
 
-    char **p, **p_op = NULL, **p_end = NULL;
-
+    char **p, **p_op = NULL;
     for (p = argv+1; *p; p++) {
-        printf("%ld -> %s -> ", p - (argv+1), *p);
         switch(gettype(*p)) {
             case NUMBER:
                 if (p_op) {
-                    puts("Numbers must appear before operators.");
+                    puts("The values must appear before operators.");
                     return ERROR;
                 }
-                puts("number");
                 break;
             case OPERATOR:
-                puts("operator");
-                p_end = p;
                 if (!p_op)
                     p_op = p;
                 break;
@@ -78,22 +88,28 @@ int32_t main(int32_t argc, char **argv) {
         return ERROR;
     }
 
-    if (p_op - (argv+1) < 2) {
-        puts("At least two numbers are required.");
+    char **p_end = argv+(argc-1);
+    if ((p_op - (argv+1)) - ((p_end - p_op) + 1) != 1) {
+        puts("the must be one less operator than value.");
         return ERROR;
     }
 
-    printf("op: %ld\n", p_op - (argv+1));
-    printf("end: %ld\n", p_end - (argv+1));
-
-    char res[1024] = {0};
-    for(char **p_nbr = p_op-1; p_nbr != argv && p_end != p_op; p_nbr-=2, p_op--) {
-        gen_eq(*(p_nbr-1), *p_nbr, *p_end, res);
-        puts(res);
+    p = p_end;
+    float result;
+    for(char **p_nbr = p_op-1; p_nbr != argv && p != p_op-1; p_nbr--, p--) {
+        if (p == p_end) {
+            result = atof(*p_nbr);
+            p_nbr--;
+        }
+        result = expr(atof(*p_nbr), result, *p[0]);
     }
 
-    puts(res);
-    
+    float f = result - (int)result;
+    if (f >= 0.00001 || f <= -0.0001)
+        printf(">> %.3f\n", result);
+    else
+        printf(">> %d\n", (int) result);
 
     return 0;
 }
+
